2ba2aa538b29ac4aef01dcc29030372c78bd476d
Grigoriy Goncharov <Gregory.Goncharov@gmail.com>
2026-09-12 14:40:41 -0400

    Sha and time werent working.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,7 @@ import { env } from '$env/dynamic/private';
 
 export const load = () => ({
   build: {
-    sha: (env.PUBLIC_GIT_SHA ?? 'dev').slice(0, 7),
-    time: env.PUBLIC_BUILD_TIME ?? 'local'
+    sha: (env.GIT_SHA ?? 'dev').slice(0, 7),
+    time: env.BUILD_TIME ?? 'local'
   }
 });
